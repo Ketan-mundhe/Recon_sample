65ef2de1abf8103eda020d990d2810ef5e1af0ac
Ketan Sunil Mundhe <64319570+Ketan-mundhe@users.noreply.github.com>
2020-08-31 13:07:12 +0530

    Update
    add sort function

--- recon.sh
+++ recon.sh
@@ -4,7 +4,7 @@ domain=$1
 wordlist="/home/ethicalman/tools/SecLists/Discovery/DNS/deepmagic.com-prefixes-top500.txt"
 resolvers="/home/ethicalman/tools/massdns/lists"
 
-echo " [*]Time to Chill...KeT4n "
+echo " [*]Time to Chill... "
 
 sub_enum(){
 
@@ -12,18 +12,24 @@ mkdir -p $domain/subdomain
 
 sublist3r -d $domain -o $domain/subdomain/sublist3r.txt
 subfinder -d $domain -o $domain/subdomain/subfinder.txt
-assetfinder -subs-only hackerone.com | tee $domain/subdomain/assetfinder.txt
-shuffledns -d $domain -w $wordlist -r $resolvers -o $domain/subdomain/shuffledns.txt
+assetfinder -subs-only $domain | tee $domain/subdomain/assetfinder.txt
+#shuffledns -d $domain -w $wordlist -r $resolvers -o $domain/subdomain/shuffledns.txt
 
 cat $domain/subdomain/*txt > $domain/subdomain/all.txt
 
 }
 sub_enum
 resolving_domains(){
-shuffledns -d $domain -list $domain/subdomain/all.txt -o $domain/subdomain/domain.txt
+#shuffledns -d $domain -list $domain/subdomain/all.txt -o $domain/subdomain/domain.txt
 }
 resolving_domains
 
 http_prob(){
-cat $domain/subdomain/domain.txt | httpx -o $domain/subdomain/final.txt
+cat $domain/subdomain/all.txt | httpx | tee $domain/subdomain/last.txt
 }
+http_prob
+
+sort_domains(){
+sort $domain/subdomain/last.txt | uniq | tee $domain/subdomain/final.txt
+}
+sort_domains
